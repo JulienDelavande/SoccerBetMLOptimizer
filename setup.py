519694d61f5d1560c506789ee4eec0fa13abe39c
Julien Delavande <jdelavande271@headmind.com>
2024-07-02 23:45:49 +0200

    change req

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ setup(
     version='0.1',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
+    include_package_data=True,
     install_requires=[],
+        package_data={
+        '': ['requirements.txt'],
+    },
 )
