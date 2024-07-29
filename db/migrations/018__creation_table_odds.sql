6d141a30a4a7b105bb47d61344dd49b412de447e
Julien Delavande <jdelavande271@headmind.com>
2024-07-29 14:47:32 +0200

    saved

--- db/migrations/018__creation_table_odds.sql
+++ db/migrations/018__creation_table_odds.sql
@@ -1,11 +1,16 @@
-CREATE TABLE IF NOT EXISTS logs (
-    id SERIAL PRIMARY KEY,
-    timestamp TIMESTAMP NOT NULL,
-    service VARCHAR(50),
-    module VARCHAR(50),
-    name VARCHAR(255),
-    level VARCHAR(50),
-    message TEXT,
-    filename VARCHAR(255),
-    lineno INT
+CREATE TABLE odds (
+    match_id VARCHAR(50),
+    sport_key VARCHAR(100),
+    sport_title VARCHAR(100),
+    commence_time TIMESTAMP,
+    home_team VARCHAR(100),
+    away_team VARCHAR(100),
+    bookmaker_key VARCHAR(50),
+    bookmaker_title VARCHAR(100),
+    bookmaker_last_update TIMESTAMP,
+    market_key VARCHAR(50),
+    market_last_update TIMESTAMP,
+    outcome_name VARCHAR(100),
+    outcome_price DECIMAL(10, 2),
+    PRIMARY KEY (match_id, bookmaker_key, market_key, outcome_name, market_last_update)
 );
