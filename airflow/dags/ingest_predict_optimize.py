a88bf2af1973d98d43d5bc45e37de3edd0588412
Julien Delavande <jdelavande271@headmind.com>
2024-09-05 16:03:49 +0200

    add protocol var

--- airflow/dags/ingest_predict_optimize.py
+++ airflow/dags/ingest_predict_optimize.py
@@ -6,12 +6,14 @@ import os
 
 DATA_INGESTION_SERVICE_NAME = os.getenv("DATA_INGESTION_SERVICE_NAME", "data_ingestion")
 DATA_INGESTION_SERVICE_NPORT = os.getenv("DATA_INGESTION_SERVICE_NPORT", "8000")
+DATA_INGESTION_SERVICE_PROTOCOL = os.getenv("DATA_INGESTION_SERVICE_PROTOCOL", "http")
 DATA_INGESTION_FBREF_ENDPOINT = os.getenv("DATA_INGESTION_FBREF_ENDPOINT", "fbref")
 DATA_INGESTION_SOFIFA_TEAM_STATS_ENDPOINT = os.getenv("DATA_INGESTION_SOFIFA_TEAM_STATS_ENDPOINT", "sofifa/team_stats")
 DATA_INGESTION_THE_ODDS_API_ODDS_ENDPOINT = os.getenv("DATA_INGESTION_THE_ODDS_API_ODDS_ENDPOINT", "the_odds_api/odds")
 
 PIPELINES_SERVICE_NAME = os.getenv("PIPELINES_SERVICE_NAME", "pipelines")
 PIPELINES_SERVICE_NPORT = os.getenv("PIPELINES_SERVICE_NPORT", "8001")
+PIPELINES_SERVICE_PROTOCOL = os.getenv("PIPELINES_SERVICE_PROTOCOL", "http")
 PIPELINES_SERVICE_INFERENCE_ENDPOINT = os.getenv("PIPELINES_SERVICE_INFERENCE_ENDPOINT", "infer")
 PIPELINES_SERVICE_OPTIMIZATION_ENDPOINT = os.getenv("PIPELINES_SERVICE_OPTIMIZATION_ENDPOINT", "optimize")
 
@@ -36,31 +38,31 @@ dag = DAG(
 
 ingest_fbref_data = BashOperator(
     task_id='ingest_fbref_data',
-    bash_command=f'curl -X GET "http://{DATA_INGESTION_SERVICE_NAME}:{DATA_INGESTION_SERVICE_NPORT}/{DATA_INGESTION_FBREF_ENDPOINT}" -H "accept: application/json"',
+    bash_command=f'curl -X GET "{DATA_INGESTION_SERVICE_PROTOCOL}://{DATA_INGESTION_SERVICE_NAME}:{DATA_INGESTION_SERVICE_NPORT}/{DATA_INGESTION_FBREF_ENDPOINT}" -H "accept: application/json"',
     dag=dag,
 )
 
 ingest_sofifa_team_stats_data = BashOperator(
     task_id='ingest_sofifa_team_stats_data',
-    bash_command=f'curl -X GET "http://{DATA_INGESTION_SERVICE_NAME}:{DATA_INGESTION_SERVICE_NPORT}/{DATA_INGESTION_SOFIFA_TEAM_STATS_ENDPOINT}" -H "accept: application/json"',
+    bash_command=f'curl -X GET "{DATA_INGESTION_SERVICE_PROTOCOL}://{DATA_INGESTION_SERVICE_NAME}:{DATA_INGESTION_SERVICE_NPORT}/{DATA_INGESTION_SOFIFA_TEAM_STATS_ENDPOINT}" -H "accept: application/json"',
     dag=dag,
 )
 
 ingest_the_odds_api_odds_data = BashOperator(
     task_id='ingest_the_odds_api_odds_data',
-    bash_command=f'curl -X GET "http://{DATA_INGESTION_SERVICE_NAME}:{DATA_INGESTION_SERVICE_NPORT}/{DATA_INGESTION_THE_ODDS_API_ODDS_ENDPOINT}" -H "accept: application/json"',
+    bash_command=f'curl -X GET "{DATA_INGESTION_SERVICE_PROTOCOL}://{DATA_INGESTION_SERVICE_NAME}:{DATA_INGESTION_SERVICE_NPORT}/{DATA_INGESTION_THE_ODDS_API_ODDS_ENDPOINT}" -H "accept: application/json"',
     dag=dag,
 )
 
 infer_match_results = BashOperator(
     task_id='infer_match_results',
-    bash_command=f'curl -X GET "http://{PIPELINES_SERVICE_NAME}:{PIPELINES_SERVICE_NPORT}/{PIPELINES_SERVICE_INFERENCE_ENDPOINT}" -H "accept: application/json"',
+    bash_command=f'curl -X GET "{DATA_INGESTION_SERVICE_PROTOCOL}://{PIPELINES_SERVICE_NAME}:{PIPELINES_SERVICE_NPORT}/{PIPELINES_SERVICE_INFERENCE_ENDPOINT}" -H "accept: application/json"',
     dag=dag,
 )
 
 optimize_bankroll_to_invest = BashOperator(
     task_id='optimize_bankroll_to_invest',
-    bash_command=f'curl -X GET "http://{PIPELINES_SERVICE_NAME}:{PIPELINES_SERVICE_NPORT}/{PIPELINES_SERVICE_OPTIMIZATION_ENDPOINT}" -H "accept: application/json"',
+    bash_command=f'curl -X GET "{PIPELINES_SERVICE_PROTOCOL}://{PIPELINES_SERVICE_NAME}:{PIPELINES_SERVICE_NPORT}/{PIPELINES_SERVICE_OPTIMIZATION_ENDPOINT}" -H "accept: application/json"',
     dag=dag,
 )
 
